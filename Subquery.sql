857aa53d67ae4df0f293d0bef2343c7e652856fb
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2025-09-06 23:45:56 +0530

    easy query

--- Subquery.sql
+++ Subquery.sql
@@ -4,3 +4,8 @@ SELECT * FROM employees e1
 WHERE 3 = ( SELECT count(e2.age) from employees e2 
 WHERE e2.age >= e1.age ); 
 
+-- Write an SQL query to find the total sales amount grouped by each customer from the Orders table.
+
+SELECT CustomerID, SUM(Amount) 
+FROM Orders 
+GROUP BY CustomerID;
