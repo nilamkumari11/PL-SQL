fdf90d222920d5677ef5a6025fbbbe496cbcaebe
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2025-09-09 20:29:22 +0530

    practice

--- Subquery.sql
+++ Subquery.sql
@@ -16,3 +16,55 @@ SELECT CustomerID, COUNT(OrderID) AS Orders
 FROM Orders
 GROUP BY CustomerID
 WHERE COUNT(OrderID) > 5;
+
+-- Retrieve the names and salaries of employees who work in the IT department and earn more than 50,000.
+
+SELECT emp_name,salary 
+FROM emp 
+WHERE emp_dept="IT" AND salary>50000;
+
+-- Find the average salary of employees in each department.
+
+SELECT dept, AVG(salary) AS avg_sal
+FROM emp 
+GROUP BY dept;
+
+-- Display the department names where the average salary is greater than 60,000.
+
+SELECT dept_name 
+FROM emp 
+GROUP BY dept_name 
+HAVING AVG(salary) > 50000;
+
+-- Retrieve the top 3 highest paid employees from the Employees table.
+
+SELECT emp_name, salary 
+FROM emp 
+ORDER BY salary DESC
+LIMIT 3;
+
+-- Display the full names of employees in the format "LastName, FirstName" and convert them to uppercase.
+
+SELECT UPPER(CONCAT(LastName,',',FirstName)) AS FulName
+FROM emp; 
+
+-- List the employees who joined in the year 2022.
+
+SELECT * 
+FROM emp 
+WHERE YEAR(joining_date) = 2022; 
+
+-- Show all employees along with their department names using a join.
+
+SELECT e.emp_name, d.dept_name 
+FROM emp e
+JOIN dept d
+ON e.dept_id = d.dept_id;
+
+-- Find the employees whose salaries are greater than the average salary of all employees.
+
+SELECT emp_name 
+FROM emp 
+WHERE salary > (SELECT AVG(salary) FROM emp); 
+
+-- Rank employees by salary within each department, showing the department name, employee name, salary, and their rank.
